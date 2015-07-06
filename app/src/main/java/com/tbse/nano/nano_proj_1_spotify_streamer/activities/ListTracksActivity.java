4202bd0d93082b770db088bf5186f9e774463076
Todd Smith <mail@toddbsmith.com>
2015-07-05 23:44:02 -0400

    removed title from dialogfragment

--- app/src/main/java/com/tbse/nano/nano_proj_1_spotify_streamer/activities/ListTracksActivity.java
+++ app/src/main/java/com/tbse/nano/nano_proj_1_spotify_streamer/activities/ListTracksActivity.java
@@ -130,5 +130,4 @@ public class ListTracksActivity extends Activity {
 
     }
 
-
 }
--- app/src/main/java/com/tbse/nano/nano_proj_1_spotify_streamer/activities/PlayTrackFragment.java
+++ app/src/main/java/com/tbse/nano/nano_proj_1_spotify_streamer/activities/PlayTrackFragment.java
@@ -1,18 +1,21 @@
 package com.tbse.nano.nano_proj_1_spotify_streamer.activities;
 
-import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.tbse.nano.nano_proj_1_spotify_streamer.R;
 import com.tbse.nano.nano_proj_1_spotify_streamer.models.TrackResult;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
+
+import kaaes.spotify.webapi.android.models.Image;
 
 @EFragment(R.layout.play_track)
 public class PlayTrackFragment extends DialogFragment {
@@ -29,12 +32,9 @@ public class PlayTrackFragment extends DialogFragment {
     }
 
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        Dialog d = super.onCreateDialog(savedInstanceState);
-
-        Log.d(TAG, "onCreateDialog");
-
-        return d;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(STYLE_NO_TITLE, getTheme());
     }
 
     @AfterViews
@@ -52,6 +52,21 @@ public class PlayTrackFragment extends DialogFragment {
         artistName.setText(tr.getTrack().artists.get(0).name);
         trackTitle.setText(tr.getTrack().name);
         albumTitle.setText(tr.getAlbum().name);
+
+        if (tr.getNumberOfImages() > 0) {
+            albumImage.setVisibility(View.VISIBLE);
+            Image image = tr.getImage();
+            if (image != null) {
+                Picasso.with(getActivity())
+                        .load(image.url)
+                        .fit()
+                        .centerCrop()
+                        .into(albumImage);
+            }
+
+        } else {
+            albumImage.setVisibility(View.INVISIBLE);
+        }
 
     }
 
